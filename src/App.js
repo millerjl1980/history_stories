100e7e447355235e217287c221e8e0f5d8711757
Justin Miller <indymiller@gmail.com>
2020-09-10 22:29:37 -0400

    updated styles from sample

--- src/App.js
+++ src/App.js
@@ -26,15 +26,23 @@ class App extends Component {
       
     <div className="App">
       {items.map(item => (
-        <div>
-        <h1>{item.attribute_values[0].value.text}</h1>
-        <img
+        <div class="row project-item">
+          <div class="col-sm-3">
+              <img
               alt={item.attribute_values[0].value.text}
               src={item.digital_assets[0].large_thumbnail}
-        />
-        <p>Date:  {item.attribute_values[1].value.date}</p>
-        <p>{item.attribute_values[3].value.text}</p>
-        </div>
+              />
+          </div>
+          <div class="col-sm-3 col">
+            {item.attribute_values[0].value.text}
+          </div>
+          <div class="col-sm-3 col">
+            {item.attribute_values[1].value.date}
+          </div>
+          <div class="col-sm-3">
+             {item.attribute_values[3].value.text}
+          </div>
+      </div>
       ))}
     </div>
     </React.Fragment>
